dd976e6041f96a6754ad1c56e3a41c43b16caca5
Miceesha <129476425+Miceesha@users.noreply.github.com>
2023-06-07 14:04:04 +0800

    Add files via upload

---
+++ index.php.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pavilion Medics</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+
+
+
+
+</head>
+
+<body>
+
+    <!--header section starts-->
+    <header class="header">
+
+        <a href="#" class="logo"><i class="fas fa-stethoscope"><i class="fas fa-heartbeat"></i></i>Pavilion Medics</a>
+
+        <nav class="navbar">
+            <a href="#home">Home</a>
+            <a href="#services">Services</a>
+            <a href="about.php">About</a>
+            <a href="#doctors">Doctors</a>
+            <a href="#specialities">Specialities</a>
+            <a href="appointment.php">Appointment</a>
+            <a href="user_login.php"><i class="fas fa-user-lock"></i></a>
+            <a href="admin_login.php"><i class="fas fa-user-tie"></i></a>
+            <a href="payment.php"><i class="fas fa-sack-dollar"></i></a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </header>
+
+    <!--header section ends-->
+
+    <!--home section starts-->
+    <section class="home" id="home">
+
+        <div class="image">
+            <img src="image/home.png" alt="">
+        </div>
+        <div class="content">
+            <h3>The First Wealth Is Health</h3>
+            <p>Stay Healthy & Stay Happy</p>
+            <a href="about.php" class="btn"> About Us <span class="fas fa-chevron-right"></span></a>
+        </div>
+    </section>
+    <!--home section ends-->
+
+    <!--icons section start-->
+    <section class="icons-container">
+        <div class="icons">
+            <i class="fas fa-user-md"></i>
+            <h3>150+</h3>
+            <p>Doctors On Duty</p>
+        </div>
+        <div class="icons">
+            <i class="fas fa-users"></i>
+            <h3>1500+</h3>
+            <p>Patients Feedbacks</p>
+        </div>
+        <div class="icons">
+            <i class="fas fa-procedures"></i>
+            <h3>500+</h3>
+            <p>Bed Facility</p>
+        </div>
+        <div class="icons">
+            <i class="fas fa-hospital"></i>
+            <h3>100+</h3>
+            <p>Pavilion Medics Branch</p>
+        </div>
+    </section>
+    <!--icons section ends-->
+
+    <!--services section starts-->
+    <section class="services" id="services">
+
+        <h1 class="heading">Our <span>Services</span></h1>
+        <div class="box-container">
+            <div class="box">
+                <i class="fas fa-notes-medical"></i>
+                <h3>Free Medical Checkup</h3>
+                <p>Pavilion Medics Offers Free Medical Checkups For Elder People</p>
+            </div>
+            <div class="box">
+                <i class="fas fa-ambulance"></i>
+                <h3>Ambulance Service</h3>
+                <p>Pavilion Medics Provide 24 Hours Of Ambulance Service</p>
+            </div>
+            <div class="box">
+                <i class="fas fa-pills"></i>
+                <h3>Free Medicines</h3>
+                <p>Pavilion Medics Offers Free Medicine For Elder People</p>
+            </div>
+            <div class="box">
+                <i class="fas fa-heartbeat"></i>
+                <h3>Nursing Care</h3>
+                <p>Pavilion Medics Offers Free Nursing Care For Elder People</p>
+            </div>
+        </div>
+
+    </section>
+    <!--services section ends-->
+
+    <!--doctors section starts-->
+    <section class="doctors" id="doctors">
+        <h1 class="heading"> Our <span>Doctors</span></h1>
+        <div class="box-container">
+            <div class="box">
+                <img src="image/doc-1.jpg" alt="">
+                <h3>Dr.Siew Lau</h3>
+                <p>Opthalmologist</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-whatsapp"></a>
+                </div>
+                <a href="doc 1.php" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+            <div class="box">
+                <img src="image/doc-2.jpg" alt="">
+                <h3>Dr.Mathew</h3>
+                <p>General Surgeon</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-whatsapp"></a>
+                </div>
+                <a href="doc 2.php" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+            <div class="box">
+                <img src="image/doc-3.jpg" alt="">
+                <h3>Dr.Sarah</h3>
+                <p>Physician</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-whatsapp"></a>
+                </div>
+                <a href="doc 3.php" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+            <div class="box">
+                <img src="image/doc-4.jpg" alt="">
+                <h3>Dr.Joe</h3>
+                <p>Cardiologist</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-whatsapp"></a>
+                </div>
+                <a href="doc 4.php" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+            <div class="box">
+                <img src="image/doc-5.jpg" alt="">
+                <h3>Dr.Malini</h3>
+                <p>Orthopedic</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-whatsapp"></a>
+                </div>
+                <a href="doc 5.php" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+            <div class="box">
+                <img src="image/doc-6.jpg" alt="">
+                <h3>Dr.Sandy</h3>
+                <p>Dentistry</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-whatsapp"></a>
+                </div>
+                <a href="doc 6.php" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+            <div class="box">
+                <img src="image/doc-7.jpeg" alt="">
+                <h3>Dr.Sanjeev</h3>
+                <p>Neurologist</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-whatsapp"></a>
+                </div>
+                <a href="doc 7.php" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+            <div class="box">
+                <img src="image/doc-8.jpeg" alt="">
+                <h3>Dr.Michele</h3>
+                <p>Urology</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-whatsapp"></a>
+                </div>
+                <a href="doc 8.php" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+        </div>
+    </section>
+    <!--doctors section ends-->
+
+    <!--specialities section starts-->
+    <section class="specialities" id="specialities">
+        <h1 class="heading"> Our <span>Specialities</span></h1>
+        <div class="box-container">
+            <div class="box">
+                <img src="image/cardio.jpeg" alt="">
+                <h3>Cardiology</h3>
+                <p>Pavilion Medics offers the best Cardilogy medical speciality</p>
+            </div>
+            <div class="box">
+                <img src="image/dentistry.jpeg" alt="">
+                <h3>Dentist</h3>
+                <p>Pavilion Medics offers the best Dental Care treatment</p>
+            </div>
+            <div class="box">
+                <img src="image/nephoro.jpeg" alt="">
+                <h3>Nephrology</h3>
+                <p>Pavilion Medics offers the best Nephrology medical speciality</p>
+            </div>
+            <div class="box">
+                <img src="image/neuro.png" alt="">
+                <h3>Neurology</h3>
+                <p>Pavilion Medics offers the best Neurology medical speciality</p>
+            </div>
+            <div class="box">
+                <img src="image/general surgery.jpeg" alt="">
+                <h3>General Surgery</h3>
+                <p>Pavilion Medics offers the best General Surgery medical speciality</p>
+            </div>
+            <div class="box">
+                <img src="image/internal medicine.jpeg" alt="">
+                <h3>Internal Medicine</h3>
+                <p>Pavilion Medics offers the best Internal Medicine medical speciality</p>
+            </div>
+            <div class="box">
+                <img src="image/oncology.jpeg" alt="">
+                <h3>Oncology</h3>
+                <p>Pavilion Medics offers the best Oncology medical speciality</p>
+            </div>
+            <div class="box">
+                <img src="image/rhemuo.jpeg" alt="">
+                <h3>Rhemuatology</h3>
+                <p>Pavilion Medics offers the best Rhemuatology medical speciality</p>
+            </div>
+        </div>
+    </section>
+    <!--specialities section ends-->
+
+    <!--footer section starts-->
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>Quick Links</h3>
+                <a href="#home"> <i class="fas fa-chevron-right"></i> Home </a>
+                <a href="#services"> <i class="fas fa-chevron-right"></i> Services </a>
+                <a href="about.html"> <i class="fas fa-chevron-right"></i> About </a>
+                <a href="#doctors"> <i class="fas fa-chevron-right"></i> Doctors </a>
+                <a href="#specialities"> <i class="fas fa-chevron-right"></i> Specialities </a>
+                <a href="#"> <i class="fas fa-chevron-right"></i> Appointment </a>
+            </div>
+            <div class="box">
+                <h3>Our Services</h3>
+                <a href="#"> <i class="fas fa-chevron-right"></i> Free Medical Checkup </a>
+                <a href="#"> <i class="fas fa-chevron-right"></i> Ambulance Service</a>
+                <a href="#"> <i class="fas fa-chevron-right"></i> Free Medicine </a>
+                <a href="#"> <i class="fas fa-chevron-right"></i> Nursing Care </a>
+            </div>
+            <div class="box">
+                <h3>Our Specialities</h3>
+                <a href="#"> <i class="fas fa-chevron-right"></i> Cardiology </a>
+                <a href="#"> <i class="fas fa-chevron-right"></i> Dentist</a>
+                <a href="#"> <i class="fas fa-chevron-right"></i> Nephrology </a>
+                <a href="#"> <i class="fas fa-chevron-right"></i> Neurology </a>
+                <a href="#"> <i class="fas fa-chevron-right"></i> General Surgery </a>
+                <a href="#"> <i class="fas fa-chevron-right"></i> Internal Medicine</a>
+                <a href="#"> <i class="fas fa-chevron-right"></i> Oncology </a>
+                <a href="#"> <i class="fas fa-chevron-right"></i> Rhemuatology </a>
+            </div>
+            <div class="box">
+                <h3>Contact Info</h3>
+                <a href="#"> <i class="fas fa-phone"></i> 03.80709876 </a>
+                <a href="#"> <i class="fas fa-phone"></i> 03.80708765 </a>
+                <a href="#"> <i class="fas fa-envelope"></i> pavilionmedics@mail.com </a>
+                <a href="#"> <i class="fas fa-map-marker-alt"></i> Technology Park Malaysia, Selangor - 47100 </a>
+            </div>
+            <div class="box">
+                <h3>Follow Us</h3>
+                <a href="#"> <i class="fab fa-facebook"></i> Facebook </a>
+                <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+                <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+                <a href="#"> <i class="fab fa-linkedin"></i> Linked In </a>
+                <a href="#"> <i class="fab fa-pinterest"></i> Pinterest </a>
+            </div>
+        </div>
+        <div class="credit"> Created By <span>Pavilion Medics</span> | All Rights Reserved | </div>
+    </section>
+    <!--footer section ends-->
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
